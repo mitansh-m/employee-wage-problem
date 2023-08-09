ac1368fad31fe463a68ff50b7ef328533b303e8a
mitansh-m <mitansh.hsnatim@gmail.com>
2023-08-09 08:40:18 +0530

    usecase 3 implemented

--- usecase1.py
+++ usecase1.py
@@ -1,18 +1,16 @@
 from random import randrange 
 
 def presence() :
-    x = randrange(0,2)
-    if x == 1 :
-        print("present")
-    else :
-        print("absent")
-    return x
+    x = randrange(0,3)
+    return x 
 
 def wage() :
     if presence() == 0 :
         x=0
-    else :
+    elif presence() == 1:
         x=20*8
+    else : 
+        x=20*4
     return x
 
 print("daily employee wage is " + str(wage()))
